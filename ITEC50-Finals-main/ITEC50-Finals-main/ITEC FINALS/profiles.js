079747e0acc207bfeca52332ed334a75983b4009
rovicpogi <122440915+rovicpogi@users.noreply.github.com>
2023-07-04 08:46:23 +0800

    Add files via upload

---
+++ ITEC50-Finals-main/ITEC50-Finals-main/ITEC FINALS/profiles.js
@@ -0,0 +1,23 @@
+function changeImage(x, image)
+{
+    if(x==1){
+        image.src = 'fb sadvoi.png';
+    }    
+    if(x==2){
+        image.src = 'ladyvj.png';
+    }
+    if(x==3){
+        image.src = 'rupert.png';
+    }
+    if(x==4){
+        image.src = 'jjpic.png';
+    }
+    if(x==5){
+        image.src = 'jezter.png';
+    }
+    if(x==6){
+        image.src = 'jimuel.png';
+    }
+    
+
+}
